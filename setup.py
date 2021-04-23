d335655feb30cb5c0136528ff9ce0b6b1673a7a9
Sebastiaan Alvarez Rodriguez van Ravensberg <sebastiaanalva@gmail.com>
2021-04-23 21:26:09 +0200

    Package requirements added

--- setup.py
+++ setup.py
@@ -1,39 +1,30 @@
 import setuptools
-import configparser
 import os
-import pathlib
-
-parser = configparser.ConfigParser()
-
-path = os.path.join(str(pathlib.Path(__file__).parent.absolute()), 'setup.cfg')
-parser.read(path)
 
 
-del parser['metadata']['long_description'] # We need to read the long description ourselves
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
+def read(fname):
+    path = os.path.join(os.path.dirname(__file__), fname)
+    f = open(path)
+    return f.read()
 
-del parser['options']['package_dir']
-package_dir = {'': 'metareserve'}
-del parser['options']['packages']
-packages=setuptools.find_packages(where='metareserve')
+install_requires = [x for x in read('requirements.txt').strip().split('\n') if x]
 
-readopts = parser['metadata']
-readopts.update(parser['options'])
-setuptools.setup(long_description=long_description, package_dir=package_dir, packages=packages, **readopts)
-# setuptools.setup(
-#     name='metareserve',
-#     version='0.1.0',
-#     author='Sebastiaan Alvarez Rodriguez',
-#     author_email='a@b.c',
-#     description='A package providing a unified reservation interface',
-#     long_description=long_description,
-#     long_description_content_type='text/markdown',
-#     url='https://github.com/Sebastiaan-Alvarez-Rodriguez/metareserve',
-#     packages=setuptools.find_packages(),
-#     classifiers=(
-#         'Programming Language :: Python :: 3',
-#         'License :: OSI Approved :: MIT License',
-#         'Operating System :: OS Independent',
-#     ),
-# )
+
+setuptools.setup(
+    name='metareserve',
+    version='0.1.0',
+    author='Sebastiaan Alvarez Rodriguez',
+    author_email='a@b.c',
+    description='A package providing a unified reservation interface',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Sebastiaan-Alvarez-Rodriguez/metareserve',
+    packages=setuptools.find_packages(where='metareserve'),
+    package_dir={'': 'metareserve'},
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+    install_requires=install_requires,
+)
